8897c7f0b4ae33915c31213ed678e3efe2bed98f
Pedro Alves <pedro22carro@gmail.com>
2020-11-18 17:04:50 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Pedindo_informacoes_do_usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string senha;
+            Console.WriteLine("Digite o seu nome de usuário");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite a sua senha");
+            senha = Console.ReadLine();
+            bool Condicao = true;
+            while(Condicao == true)
+            {
+                if (nome == senha)
+                {
+                   Console.WriteLine("Erro, você digitou a senha igual ao nome de usuário");
+                   Console.WriteLine("Digite o seu nome de usuário");
+                    nome = Console.ReadLine();
+                    Console.WriteLine("Digite a sua senha");
+                    senha = Console.ReadLine();
+                }else
+                {
+                    Console.WriteLine("Você digitou o nome de usuário e a senha corretamente");
+                    Condicao = false;
+                }
+            }
+        }
+    }
+}
